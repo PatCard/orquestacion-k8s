9605dc03394caeafabb3712d554dc9ac0dcc1698
Patricio <patcard@hotmail.com>
2026-05-19 22:47:17 -0400

    fix: actualizar stop.sh para cerrar todos los puertos

--- stop.sh
+++ stop.sh
@@ -3,10 +3,10 @@
 echo "🛑 Deteniendo entorno K8s..."
 
 # Detener port-forwards
-echo "🔌 Cerrando port-forwards..."
-kill $(lsof -ti:9090) 2>/dev/null
+echo "🔌 Cerrando puertos..."
+kill $(lsof -ti:80) 2>/dev/null
 kill $(lsof -ti:8000) 2>/dev/null
-kill $(lsof -ti:8080) 2>/dev/null
+kill $(lsof -ti:9090) 2>/dev/null
 
 # Detener cluster
 echo "⏳ Deteniendo minikube..."
